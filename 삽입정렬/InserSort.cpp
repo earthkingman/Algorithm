81c14eca9a3037cbe9e02188200ba1b63afe693c
qkrskdjf <qkrcksdjf1004@gmail.com>
2020-02-14 18:50:15 +0900

    삽입정렬

---
+++ 삽입정렬/InserSort.cpp
@@ -0,0 +1,34 @@
+//버블 정렬 (오름차순) 
+
+#include <stdio.h>
+
+
+ 
+ int main(void){
+ 
+ int i , j  ,temp ;
+
+int array[10] ={ 1 , 10,5,8,7,6,4,3,2,9};
+ 
+ 
+ for(i=0; i<9 i++){
+ 	
+j=i;
+while(array[j]>array[j+1]{
+
+temp =array[j];
+array[j+1]=array[j];
+array[j+1]=temp;
+j--
+}
+
+)
+
+ }
+
+return 0;
+ 
+ } 
+ 
+ 
+ 
