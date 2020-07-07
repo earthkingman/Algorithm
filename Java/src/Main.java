4e988a88f2c5ef09059cc5fb3ef8b63f0727e079
qkrskdjf <qkrcksdjf1004@gmail.com>
2020-07-07 20:23:53 +0900

    선택정렬 자바

---
+++ Java/src/Main.java
@@ -0,0 +1,52 @@
+
+public class Main {
+
+	private static void selectionSort(int []arr) {
+		
+		selectionSort(arr,0);
+		
+	}
+	
+private static void selectionSort(int []arr,int start) {
+		
+		if(start < arr.length-1) {
+			
+			int min_index=start;
+			
+			for(int i=start; i<arr.length; i++) {
+				if(arr[i]<arr[min_index]) min_index=i;
+			}
+			swap(arr,start,min_index);
+			selectionSort(arr,start+1);
+		}
+	}
+
+
+	
+private static void swap(int[] arr, int start, int min_index) {
+	int tmp =arr[start];
+	arr[start]=arr[min_index];
+	arr[min_index]=tmp;
+	
+}
+
+private static void printArray(int[] arr) {
+
+	for(int data : arr) {
+		System.out.print(data+" ");
+		
+	}
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+int a[]= {5,3,2,6,7,1,4};
+
+		printArray(a);
+		selectionSort(a);
+		System.out.print("\n");
+		printArray(a);
+	}
+
+}
