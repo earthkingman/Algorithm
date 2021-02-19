3084573333a49b5b48d24db3f07e5fdc42481861
ji-park <ji-park@ji-parkui-MacBookAir.local>
2021-02-20 01:18:49 +0900

    #18층간소음

--- C/18.c
+++ C/18.c
@@ -5,14 +5,24 @@ int main(void)
     int n, m;
     int max_time;
     int count;
+    int sensor[10] = {23,17,120,34,112,136,123,23,25,113};
 
     max_time = 0;
     count = 0;
-    scanf("%d", &n);
+    n = 10;
+    m = 90;
+
     for (int i = 0; i < n; i++)
     {
-        scanf("%d", &m);
-        if ()
+        if (m < sensor[i])
+            {   
+                count++;
+                if (max_time < count)
+                    max_time = count;
+            }
+        else
+            count = 0;
     }
+    printf("%d", max_time);
     return (0);
 }
