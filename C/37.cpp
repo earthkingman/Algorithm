c06a900471f790d8454d9cce2254227511e1c7aa
ji-park <qkrskdjf@naver.com>
2021-03-02 19:07:36 +0900

    #37

--- C/37.cpp
+++ C/37.cpp
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-void check(vector <int> a, int num)
+void check(vector<int> &a, int num)
 {
     for (int i = 0; i < a.size(); i++)
     {
         if (a[i] == num)
-            {
-                a.erase(a.begin() + i);
-                return ;
-            }
+        {
+            a.erase(a.begin() + i);
+            return;
+        }
     }
-    a.erase(a.end() - 1);
+    a.pop_back();
 }
 
 int main()
 {
-    int n,s;
+    int n, s;
     int num;
 
     scanf("%d %d", &n, &s);
-    vector <int> a(n, 0);
+    vector<int> a(n, 0);
 
     for (int i = 0; i < s; i++)
     {
         scanf("%d", &num);
         check(a, num);
-        a.insert(a.begin(),num);
-        
+        a.insert(a.begin(), num);
     }
-    for (int i = 0 ; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("%d ", a[i]);
     }
